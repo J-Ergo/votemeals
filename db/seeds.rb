81726b23669eb92fd20a84f06c94726e3d5a75d9
Jesus Ablan <ablan.jesus@gmail.com>
2016-04-25 12:22:03 -0400

    topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,14 @@ require 'faker'
    user.save!
  end
  users = User.all
+
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
@@ -24,6 +32,7 @@ require 'faker'
  50.times do
    Post.create!(
      user:   users.sample,
+     topic:  topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -71,3 +80,4 @@ require 'faker'
  puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Topic.count} topics created"
